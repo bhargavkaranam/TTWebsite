c21b2badaa3073abe7a74ebf81585f7ea896488d
Divyanshu <divyanshu.1996@gmail.com>
2016-07-18 00:11:24 +0530

    added contact us page

--- TT16/fullPage.js-master/script.js
+++ TT16/fullPage.js-master/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function(){
 	$(".closeBttn").click(function(){
  $(".sidenav").css("width","0");
@@ -8,10 +7,4 @@ $(document).ready(function(){
 		// $(".openBttn").css("transform","rotate(360deg)");
 	});
 
-=======
-fullpage.initialize('#fullpage',{
-	anchors: ['section1','section2','section3','section4','footer'],
-	menu:'#menu',
-	css3:true;
->>>>>>> f91c9e334fca5440b311c0857080612ecb9233d3
 });
